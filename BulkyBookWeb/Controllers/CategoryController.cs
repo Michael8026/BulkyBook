03b818a9908747482bc556f181d9c6e6623f118f
m.sogunro <m.sogunro@lagetronix.com>
2023-05-26 17:04:36 +0100

    Create Action and Controller and Val;idations

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+                
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<Category> objCategoryList = _context.Categories;
+            
+            return View(objCategoryList);
+        }
+
+
+        //GET
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
         }
     }
 }
