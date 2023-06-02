640a1edc39415089d283c19caf804b3b4bcea080
m.sogunro <m.sogunro@lagetronix.com>
2023-06-02 16:34:03 +0100

    Worked on Create View and Controller

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -32,6 +32,12 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            //to create a custom validation (It displays if theres a validation summary)
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the name"); //if we customised the "CustomError" to the name of a property it displays it under the property field
+            }
+
             if (ModelState.IsValid)
             {
                 _context.Categories.Add(obj);
@@ -40,6 +46,42 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
             
+        }
+
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var dbCategory = _context.Categories.Find(id);
+
+
+            return View();
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            //to create a custom validation (It displays if theres a validation summary)
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the name"); //if we customised the "CustomError" to the name of a property it displays it under the property field
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
         }
     }
 }
